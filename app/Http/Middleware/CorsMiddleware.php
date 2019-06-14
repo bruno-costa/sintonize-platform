2a4d4af44b8034864da694e5840de365eee33c97
Bruno Avelino <brunoac.cc@gmail.com>
2019-06-14 17:37:53 -0300

    cors v2

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,23 +9,26 @@ class CorsMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        $provider = $request->header('Access-Control-Request-Headers');
+        $headers = [
+            'Access-Control-Allow-Headers' => $provider,
+            'Access-Control-Allow-Methods' => 'POST, GET',
+            'Access-Control-Expose-Headers' => 'api-version',
+            'Access-Control-Allow-Origin' => $request->header('Origin', '*'),
+        ];
         if ($request->isMethod('OPTIONS')) {
-            $provider = $request->header('Access-Control-Request-Headers');
-            $headers = [
-                'Access-Control-Allow-Headers' => $provider,
-                'Access-Control-Allow-Methods' => 'POST, GET',
-                'Access-Control-Expose-Headers' => 'api-version',
-                'Access-Control-Allow-Origin' => $request->header('Origin', '*'),
-            ];
             return response("", 204, $headers);
         }
         $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
         $response->headers->set('api-version', '1.1.0');
         return $response;
     }
